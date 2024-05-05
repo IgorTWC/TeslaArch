0b26e5b49b32da66d0e94b4df131d2365b80698a
Xortfo <igorlawa@gmail.com>
2024-05-05 16:30:20 +0200

    x

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,4 @@
-var correct = document.getElementById("correct");
+    var correct = document.getElementById("correct");
 var incorrect = document.getElementById("incorrect");
 function correctAnwser() {
     document.getElementById("display").innerHTML = '<a style="color:#0f0;" href="index.html">Continue onwards </a>';
