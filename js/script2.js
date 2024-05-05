e3120fd9d0570f7ed384a77b11086a892d1f3738
Xortfo <igorlawa@gmail.com>
2024-05-05 15:49:03 +0200

    asd

--- js/script2.js
+++
@@ -1,8 +0,0 @@
-var correct = document.getElementById("correct");
-var incorrect = document.getElementById("incorrect");
-function correctAnwser() {
-    document.getElementById("display").innerHTML = '<a style="color:#0f0;" href="index.html">Continue onwards </a>';
-}
-function incorrectAnwser() {
-    document.getElementById("display").innerHTML = "Try again.";
-}
